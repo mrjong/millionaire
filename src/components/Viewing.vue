27e32ad4bc90f7063dad52dbd81afd3c979fc031
zhangpanpan <zhangpanpan@apusapps.com>
2018-03-03 13:52:12 +0800

    4.0版本样式修正

--- src/components/Viewing.vue
+++ src/components/Viewing.vue
@@ -20,17 +20,13 @@ export default {
     height: 50px;
     border-radius: 26px;
     border:1px solid #cac6d8;
-    line-height: 45px;
-    vertical-align: middle;
     padding:0 16px;
     display: flex;
     &__icon{
       width: 30px;
       align-self: center;
-      display: flex;
       img{
         width: 100%;
-        align-self: center;
       }
     }
     &__text{
