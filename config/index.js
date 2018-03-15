dd8e303dabc914fc0d690de2a9119d5e6ea1983f
lixuan <lixuan@apusapps.com>
2018-03-15 13:57:49 +0800

    swtich to test

--- config/index.js
+++ config/index.js
@@ -14,7 +14,7 @@ module.exports = {
 
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
-    // host: '192.168.221.145',
+    // host: '192.168.221.134',
     port: 8080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: true,
     errorOverlay: true,
@@ -53,7 +53,7 @@ module.exports = {
     assetsSubDirectory: 'static',
     // assetsPublicPath: '/', // 正式
     // assetsPublicPath: '/preview/millionaire/', // 测试
-    assetsPublicPath: '/preview/millionaire/', // 测试
+    assetsPublicPath: '/act/millionaire/', // 测试
 
     /**
      * Source Maps
