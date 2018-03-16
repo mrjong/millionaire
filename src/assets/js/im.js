fad46a2b28faa231b8e0689a15e74f3516756e11
lixuan <lixuan@apusapps.com>
2018-03-16 15:46:39 +0800

    修复im重新连接，添加重新连接频率

--- src/assets/js/im.js
+++ src/assets/js/im.js
@@ -167,25 +167,26 @@ const im = {
    * 重新连接
    */
   reconnect () {
+    const self = this
     const callback = {
       onSuccess: (userId) => {
         console.log('Reconnect successfully.' + userId)
-        this.emitListener(type.CONNECT_SUCCESS)
+        self.emitListener(type.CONNECT_SUCCESS)
       },
       onTokenIncorrect: function () {
         console.log('token无效')
-        this.emitListener(type.CONNECTED_ERROR, 'token无效')
-        this.emitListener(type.INVALID_TOKEN)
+        self.emitListener(type.CONNECTED_ERROR, 'token无效')
+        self.emitListener(type.INVALID_TOKEN)
       },
       onError: (errorCode) => {
         console.log(`重新连接出错 错误码：${errorCode}`)
-        this.emitListener(type.CONNECTED_ERROR, `重新连接出错 错误码：${errorCode}`)
+        self.emitListener(type.CONNECTED_ERROR, `重新连接出错 错误码：${errorCode}`)
       }
     }
     const config = {
       auto: true,
       url: 'cdn.ronghub.com/RongIMLib-2.2.6.min.js',
-      rate: [100, 1000, 2000, 3000, 30000]
+      rate: [500, 1000, 2000, 3000, 5000, 8000, 10000, 20000]
     }
     RongIMClient.reconnect(callback, config)
   },
