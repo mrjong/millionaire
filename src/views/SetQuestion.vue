46e7c2751dc2563e6deb29fd64bb0eb8c0bcc5c2
zhangpanpan <zhangpanpan@apusapps.com>
2018-03-23 18:09:33 +0800

    出题页面ui完成

--- src/views/SetQuestion.vue
+++ src/views/SetQuestion.vue
@@ -1,6 +1,51 @@
 <template>
   <div class="set-question">
-    <div class="set-question__wrap"></div>
+    <div class="set-question__wrap" v-if="!isPop">
+      <div class="back">
+        <p class="back__icon icon-fanhui iconfont"></p>
+      </div>
+      <p class="set-question__wrap__title">Set Questions Myself</p>
+      <div class="form">
+        <input type="text" class="form__name base" placeholder="YOUR NAME  (optional)">
+        <div class="form__question">
+          <p class="form__question__hint">
+            <span class="form__question__hint__icon icon-yonghuchuti_qianzise iconfont"></span>
+            Tap to set your question now
+          </p>
+          <textarea class="" maxlength="100"></textarea>
+        </div>
+        <input type="text" id="answerA"  class="base answer-text" placeholder="Option A" maxlength="25">
+        <input type="text" id="answerB"  class="base answer-text" placeholder="Option B" maxlength="25">
+        <input type="text" id="answerC"  class="base answer-text" placeholder="Option C" maxlength="25">
+        <div class="form__correct">
+          <div class="form__correct__title">
+            <p class="form__correct__title__icon iconfont icon-duigou"></p>
+            <p class="form__correct__title__text">Correct answer</p>
+          </div>
+          <div class="form__correct__options">
+            <p class="option-a">
+              <span>A</span>
+              <label for="optionA"  class=" base-option iconfont">
+              <input type="radio" id="optionA">
+            </label>
+            </p>
+            <p class="option-b">
+              <span>B</span>
+              <label for="optionB"  class=" base-option iconfont">
+              <input type="radio" id="optionB">
+            </label>
+            </p>
+            <p class="option-c">
+              <span>C</span>
+              <label for="optionC"  class=" base-option iconfont">
+              <input type="radio" id="optionC">
+            </label>
+            </p>
+          </div>
+        </div>
+        <div class="form__submit">Submit</div>
+        </div>
+    </div>
     <div class="set-question__mark" v-if="isPop">
       <div class="bomb">
         <img src="../assets/images/logo.png" class="bomb__logo">
@@ -38,7 +83,7 @@ export default {
       isPop: true
     }
   },
-  mounted() {
+  mounted () {
     this.isPop = true
   },
   methods: {
@@ -55,6 +100,149 @@ export default {
   background: url("../assets/images/set-question-bg.jpg") no-repeat top left;
   background-size: cover;
   color: #fff;
+  &__wrap{
+    height: 100%;
+    position: relative;
+    padding: 24px 30px 0;
+    .back{
+      width: 54px;
+      height: 54px;
+      background-color: rgba(36, 18, 98, 0.2);
+      border-radius: 50%;
+      position: absolute;
+      top: 24px;
+      left: 30px;
+      display: flex;
+      justify-content: center;
+      &__icon{
+        font-size: 32px;
+        align-self: center;
+        color:#241262 ;
+      }
+    }
+    &__title{
+      font: 36px "Roboto-Medium";
+      color: #241262;
+      text-align: center;
+    }
+    .form{
+      padding: 24px 0;
+      input.answer-text {
+      }
+      input.answer-text::-webkit-input-placeholder{
+        text-align: left;
+      }
+      &__question {
+        width:656px ;
+        height: 334px;
+        background: url("../assets/images/question-area-bg.png") no-repeat top left;
+        background-size: cover;
+        border-radius: 26px;
+        margin-bottom: 20px;
+        color:#241262 ;
+        font-size: 36px;
+        position: relative;
+        &__hint{
+          width: 100%;
+          height: 54px;
+          line-height: 54px;
+          position: absolute;
+          top: 50%;
+          left: 50%;
+          transform: translate(-50%,-50%);
+          color: #241262;
+          opacity: 0.6;
+          text-align: center;
+          font-size: 32px;
+          &__icon{
+            display: inline-block;
+            height: 54px;
+            line-height: 54px;
+            color: #241262;
+            font-size: 32px;
+          }
+        }
+        textarea {
+          width:100% ;
+          height: 100%;
+          border: 0;
+          padding:10px 0 10px 30px;
+          background-color: transparent;
+        }
+        textarea:focus {
+          outline: 0;
+        }
+      }
+      &__correct{
+        width: 656px;
+        height: 192px;
+        background-color: #f4f3f7;
+        border-radius: 26px;
+        &__title {
+          display: flex;
+          padding:24px 0 0;
+          justify-content: center;
+          &__icon{
+            width: 34px;
+            height: 32px;
+            text-align: center;
+            font-size: 15px;
+            color: #241262;
+            line-height: 34px;
+            box-shadow: 0 0 0 1px #241262;
+            border-radius: 5px;
+            align-self: center;
+            margin-right: 20px;
+          }
+          &__text{
+            color: #241262;
+            line-height: 54px;
+            font: 36px "Roboto-Regular";
+          }
+        }
+        &__options{
+          display: flex;
+          justify-content: center;
+          width: 80%;
+          margin: 36px auto 0;
+          .option-a,.option-b,.option-c{
+            flex: 1;
+            display: flex;
+            justify-content: center;
+            span{
+              font-size: 36px;
+              color: #241262;
+              margin-right: 24px;
+            }
+            input{
+              display: none;
+            }
+            .base-option {
+              width: 34px;
+              height: 32px;
+              text-align: center;
+              font-size: 15px;
+              color: #241262;
+              line-height: 34px;
+              box-shadow: 0 0 0 1px #241262;
+              border-radius: 5px;
+              align-self: center;
+            }
+          }
+        }
+      }
+      &__submit{
+        width: 656px;
+        height: 93px;
+        line-height: 93px;
+        background-color: rgba(250, 167, 23, 0.95);
+        text-align: center;
+        border-radius: 46px;
+        font-size: 36px;
+        margin-top: 24px;
+      }
+    }
+  }
   &__mark{
     width: 100%;
     height: 100%;
@@ -119,6 +307,24 @@ export default {
         }
       }
     }
+  }
+  .base {
+    width:656px ;
+    height: 93px;
+    border-radius: 46px;
+    background-color:#f4f3f7 ;
+    border: 0;
+    margin-bottom: 20px;
+    padding-left: 30px;
+    color:#241262 ;
+    font-size: 36px;
+  }
+  input::-webkit-input-placeholder{
+    width: 100%;
+    color: #241262;
+    opacity: 0.6;
+    text-align: center;
+    font-size: 32px;
   }
 }
 </style>
