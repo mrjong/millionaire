35061054b6ad0d0e754015113f0f1a8b3cb22e9a
unknown <zhangpanpan@corp.apus.com>
2018-06-28 16:35:00 +0800

    添加多语言文案

--- src/i18n/lang/in.js
+++ src/i18n/lang/in.js
@@ -6,6 +6,7 @@ const locale = {
     userinfo_blance_text: 'Balance',
     userinfo_rank_text: 'Weekly Rank',
     extra_lives_text: 'EXTRA LIVES',
+    get_more_text: 'Get More',
     referral_code_btn: 'Apply Referral Code',
     invite_btn: 'Invite & Earn Cash',
     play_card_title: 'HOW TO PLAY',
@@ -14,20 +15,134 @@ const locale = {
     play_card_step3: 'Use {life} when answering incorrectly. Invite new user to get it!',
     ses_question_btn: 'Set Questions Myself'
   },
-  balance: {},
-  balanceDetail: {},
-  balanceRecord: {},
-  blank: {},
-  contact: {},
+  balance: {
+    your_blance: 'Your Balance',
+    total_revenus: 'Total Revenus',
+    hint: 'You can cash out with the minimum balance of ₹150,The payouts will be made in 7 days after Approved',
+    cash_out_btn: 'Cash Out'
+  },
+  balanceDetail: {
+    title: 'Cash Out',
+    withdraw_cash_text: 'Withdraw Cash',
+    name: 'Name',
+    p_number: 'Permanent Account Number',
+    paytm: 'Paytm Account',
+    noitce: 'Notice: The info above will be submitted to paytm for cash out. Currently, only India rupee are available to be withdrawn in our game.',
+    cash_out_btn: 'Cash Out',
+    balance_pop: {
+    }
+  },
+  balanceRecord: {
+    withdrawal_title: 'Withdrawal History',
+    cash_title: 'Cash History',
+    item_withdrawal_title: 'Cash out',
+    item_cash_title: 'Win Cash',
+    delete: 'Delete All',
+    no_withdrawal: 'No Withdrawal History',
+    no_case: 'No Cash History',
+    faild_hint: 'Sorry,your apply for cash out was failed. Please check and submit again.',
+    cash_status: {
+      withdrawing: 'Withdrawing',
+      approved: 'Approved',
+      failed: 'Failed',
+      success: 'Success'
+    },
+    delete_pop: {
+      delete_withdwawal: 'Are you sure you want to delete all the withdrawal history?',
+      delte_cash: 'Are you sure you want to delete all the cash history?',
+      delete_faild: 'You failed to delete the Withdrawal History.'
+    }
+  },
+  blank: {
+    text1: 'Sorry!',
+    text2: 'Currently, we don\'t provide any game service in EU, and it is only available in India.'
+  },
+  contact: {
+    title: 'Contact Us',
+    describe: 'If you have any question, or you want to delete your account permanently, feel free to contact us by the following methods:',
+    email: 'gomillionaire@apusapps.com',
+    fb: 'Facebook',
+    twitter: 'Twitter',
+    ins: 'Instagram'
+  },
   invite: {},
   login: {},
   main: {},
   rank: {},
   rule: {},
-  setQuestion: {},
-  setQuestionResult: {},
-  userCenter: {},
-  shareDetail: {},
+  setQuestion: {
+    title: 'Set Questions Myself',
+    share_btn: 'Follow Us',
+    name: 'YOUR NAME',
+    tel: 'YOUR PHONE NUMBER',
+    question: 'Tap to set your question now',
+    option_a: 'Option A',
+    option_b: 'Option B',
+    option_c: 'Option C',
+    correct_text: 'Correct answer',
+    submit_btn: 'Submit',
+    question_rule: {
+      title1: 'QUIZ MASTER Required!',
+      title2: 'Set it, Question it, Win it!!',
+      instruction: [
+        '1. Set questions with proper grammar and spelling, and submit them to be our QUIZ MASTER. ',
+        '2. Once your questions are selected, you\'ll get the chances to answer your own question in \'Go! Millionaire\' Game to win.',
+        '3. Once your questions are selected, you\'ll get the chances to answer your own question in \'Go! Millionaire\' Game to win.',
+        '4. Your name will be featured below your questions, which means to be seen by millions of APUS Users.',
+        '5. We\'ll announce each QUIZ MASTER on @APUSBrowser Facebook page everyday.'
+      ],
+      notice: {
+        title: 'Note:',
+        text1: 'English questions only.',
+        text2: 'Submit as more questions as you can to be "Golden QUIZ',
+        text3: 'MASTER" every week, awards await you.'
+      },
+      agree_btn: 'OK, I agree'
+    },
+    question_pop: {
+      no_name: 'Please enter your name.',
+      no_tel: 'Please enter your phone number',
+      wrong_tel: 'Please enter right phone number',
+      no_question: 'Please complete the question',
+      no_answer: 'Please complete the answer',
+      submit_faild: 'Your Internet is unstable, please try it again.'
+    }
+  },
+  setQuestionResult: {
+    title: 'Set Questions Myself',
+    text1: 'Your question has been submitted.',
+    text2: 'Thank you! Knowledge is power. We\'ll reeview all submissions and announce the seleted questions and its QUIZ MASTER on APUS Browser Facebook page every day.',
+    text3: 'You will acquire:',
+    text4: '1. Hints for quiz every day',
+    text5: '2. More interesting contests to win prize',
+    text6: '3. Chance to join Quiz Master Group',
+    share_btn: 'Follow Us',
+    submit_btn: 'Submit another'
+  },
+  userCenter: {
+    title: 'User Profile',
+    name: 'Nickname',
+    tel: 'Phone Number',
+    code: 'Referral Code',
+    cash_history: 'Cash History',
+    contact: 'Contact Us',
+    logout_btn: 'Log Out',
+    logout_pop: {
+      text1: 'Are you sure you want to log out?',
+      text2: 'You failed to logout, please try again.'
+    }
+  },
+  shareDetail: {
+    title: 'Extra Lives',
+    describe1: 'You can use EXTRA LIVES when you answer incorrectly after logging in. It will be applied automatically. Two extra lives could be used per game except the last question.',
+    describe2: 'You can get it through INVITING a new user. Every time he/she signs up with your Referral Code and play our game, both of you get one.',
+    text: 'My Referral Code:',
+    share_btn: 'Invite',
+    share_pop: {
+      text1: 'My Referral Code:',
+      text2: 'Inviting friends to get it now!'
+    }
+  },
   publicText: {
     policy_text: '{agreement} & {policy}'
   }
